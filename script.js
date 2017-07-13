fc737d17f6f26e3fd24949bf43f2f09b55a4b18a
TiagoDanin <TiagoDanin@outlook.com>
2017-07-13 12:47:33 -0300

    Init JUnicodeInfo

---
+++ script.js
@@ -0,0 +1,13 @@
+function unicodeCharacter () {
+	var text = document.getElementsByClassName("StringText")[0].value;
+	var html = "";
+	var char = "";
+	text = text.toString();
+	var urlFileFormat = "http://www.fileformat.info/info/unicode/char/";
+	for (var i=0; i < text.length; i++) {
+		char = ((text.charCodeAt(i)).toString(16)).toString();
+		html += "<li><a href=\"" + urlFileFormat + char + "/index.htm\"><i class=\"ti-align-justify\">" + (text[i]).toString() + "(" + char + ")</i></a></li>";
+		text.charCodeAt(i).toString(16);
+	}
+	document.getElementsByClassName("Unicodes")[0].innerHTML = html;
+}
