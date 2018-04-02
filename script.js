3f435a197b03c99c4ad619bdffb6196f96df818d
TiagoDanin <TiagoDanin@outlook.com>
2018-04-02 10:02:10 -0300

    Update page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function unicodeCharacter () {
 	var urlFileFormat = "http://www.fileformat.info/info/unicode/char/";
 	for (var i=0; i < text.length; i++) {
 		char = ((text.charCodeAt(i)).toString(16)).toString();
-		html += "<li><a href=\"" + urlFileFormat + char + "/index.htm\"><i class=\"ti-align-justify\">" + (text[i]).toString() + "(" + char + ")</i></a></li>";
+		html += "<a href=\"" + urlFileFormat + char + "/index.htm\"><i class=\"ti-align-justify\">" + (text[i]).toString() + "(" + char + ")</i></a>  ";
 		text.charCodeAt(i).toString(16);
 	}
 	document.getElementsByClassName("Unicodes")[0].innerHTML = html;
